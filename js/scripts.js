d68469e9500e08060d676282e53a9632a29bfe3a
agatakolohe <agatakolohe@gmail.com>
2020-10-22 11:37:38 -0700

    revise wiki to be animal quiz, create questions, divs for bear, dolphin, dog results, in css hide divs, add jumbotron, create branch tree in js to generate correct answers from user inputs
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
   $("form#critters").submit(function (event) {
     event.preventDefault();
-    const animal = $("input:radio[name=animal]:checked").val();
+    const home = $("input:radio[name=home]:checked").val();
+    const food = $("input:radio[name=food]:checked").val();
+    const legs = $("input:radio[name=legs]:checked").val();
+    const humans = $("input:radio[name=humans]:checked").val();
 
-    if (animal === "cat") {
+    if (home === "house" && food === "fish") {
       $(".cat-wiki").show();
-      $(".rac-wiki, .octo-wiki").hide();
-    } else if (animal === "raccoon") {
+      $(".rac-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
+    } else if (home === "forest") {
+      $(".bear-wiki").show();
+      $(".cat-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .rac-wiki").hide();
+    } else if (legs === "none") {
+      $(".dolphin-wiki").show();
+      $(".cat-wiki, .octo-wiki, .dog-wiki, .rac-wiki, .bear-wiki").hide();
+    } else if (home === "house" && food === "everything") {
+      $(".dog-wiki").show();
+      $(".cat-wiki, .octo-wiki, .rac-wiki, .dolphin-wiki, .bear-wiki").hide();
+    } else if (home === "trash") {
       $(".rac-wiki").show();
-      $(".cat-wiki, .octo-wiki").hide();
+      $(".cat-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
     } else {
       $(".octo-wiki").show();
-      $(".rac-wiki, .cat-wiki").hide();
+      $(".cat-wiki, .rac-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
     }
   });
 });
