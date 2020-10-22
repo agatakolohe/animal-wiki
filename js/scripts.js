3fc41fbb5d7a1d45768f12810dd704e2c3381cfa
agatakolohe <agatakolohe@gmail.com>
2020-10-22 14:08:42 -0700

    change values to numbers, parseint input values, create result var, change branch to number range
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
   $("form#critters").submit(function (event) {
     event.preventDefault();
-    const home = $("input:radio[name=home]:checked").val();
-    const food = $("input:radio[name=food]:checked").val();
-    const legs = $("input:radio[name=legs]:checked").val();
-    const humans = $("input:radio[name=humans]:checked").val();
+    const home = parseInt($("input:radio[name=home]:checked").val());
+    const food = parseInt($("input:radio[name=food]:checked").val());
+    const legs = parseInt($("input:radio[name=legs]:checked").val());
+    const humans = parseInt($("input:radio[name=humans]:checked").val());
+    const result = home + food + legs + humans;
 
-    if (home === "house" && food === "fish") {
-      $(".cat-wiki").show();
-      $(".rac-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
-    } else if (home === "forest") {
-      $(".bear-wiki").show();
-      $(".cat-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .rac-wiki").hide();
-    } else if (legs === "none") {
-      $(".dolphin-wiki").show();
-      $(".cat-wiki, .octo-wiki, .dog-wiki, .rac-wiki, .bear-wiki").hide();
-    } else if (home === "house" && food === "everything") {
-      $(".dog-wiki").show();
-      $(".cat-wiki, .octo-wiki, .rac-wiki, .dolphin-wiki, .bear-wiki").hide();
-    } else if (home === "trash") {
+    if (result >= 2000) {
       $(".rac-wiki").show();
       $(".cat-wiki, .octo-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
-    } else {
+    } else if (result >= 1000) {
+      $(".dog-wiki").show();
+      $(".cat-wiki, .octo-wiki, .bear-wiki, .dolphin-wiki, .rac-wiki").hide();
+    } else if (result >= 750) {
+      $(".bear-wiki").show();
+      $(".cat-wiki, .octo-wiki, .dog-wiki, .rac-wiki, .dolphin-wiki").hide();
+    } else if (result >= 300) {
       $(".octo-wiki").show();
-      $(".cat-wiki, .rac-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
+      $(".cat-wiki, .dog-wiki, .rac-wiki, .dolphin-wiki, .bear-wiki").hide();
+    } else if (result >= 250) {
+      $(".dolphin-wiki").show();
+      $(".cat-wiki, .octo-wiki, .dog-wiki, .rac-wiki, .bear-wiki").hide();
+    } else {
+      $(".cat-wiki").show();
+      $(".octo-wiki, .rac-wiki, .dog-wiki, .dolphin-wiki, .bear-wiki").hide();
     }
   });
 });
